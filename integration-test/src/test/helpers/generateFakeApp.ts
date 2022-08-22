c8ea05a92bfc206c2448187bf80c4bebf8e849e1
Ryosuke Iwanaga <ryosuke.iwanaga@autify.com>
2022-08-22 18:01:52 +0000

    Run `generate-fake-app` before running `jest`
    Since the current code has a tiny race condition when creating fake apps
    because `jest` runs multiple test files in parallel.
    
    This commit introduces a new npm task to generate fake apps and
    run it before running `jest` so that we can ensure that this process
    runs only once.

---
+++ integration-test/src/test/helpers/generateFakeApp.ts
@@ -0,0 +1,25 @@
+/* eslint-disable unicorn/filename-case */
+import { existsSync, mkdirSync, writeFileSync } from "node:fs";
+import { join } from "node:path";
+import { androidBuildPath, iosBuildPath } from "./testAutifyCliSnapshot";
+
+// https://commons.wikimedia.org/wiki/File:Transparent.gif
+const tinyBinary = Buffer.from(
+  "R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7",
+  "base64"
+);
+
+// Generate fake build files if not exiting.
+// For recording, you should setup the real files on the same paths.
+if (existsSync(iosBuildPath)) {
+  console.log(`${iosBuildPath} already exists.`);
+} else {
+  mkdirSync(iosBuildPath); // *.app is a directory
+  writeFileSync(join(iosBuildPath, "ios"), tinyBinary); // Add a fake binary file
+}
+
+if (existsSync(androidBuildPath)) {
+  console.log(`${androidBuildPath} already exists.`);
+} else {
+  writeFileSync(androidBuildPath, tinyBinary); // Create a fake binary file
+}
