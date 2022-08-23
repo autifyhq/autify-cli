2a11bb9835b8924513c75d874c8042730688eb7e
Ryosuke Iwanaga <ryosuke.iwanaga@autify.com>
2022-08-23 22:33:53 +0000

    WIP

---
+++ integration-test/src/test/golden/mobileTestRunIosWait.test.ts
@@ -0,0 +1,6 @@
+/* eslint-disable unicorn/filename-case */
+import { testAutifyCliSnapshot } from "../helpers/testAutifyCliSnapshot";
+
+testAutifyCliSnapshot(
+  "mobile test run https://mobile-app.autify.com/projects/AAA/test_plans/BBB --build-path /path/to/MyApp.app --wait"
+);
