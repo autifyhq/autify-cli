7616b3452a5ccfca588e56b1ef8b314d00dba3c2
Ryosuke Iwanaga <ryosuke.iwanaga@autify.com>
2022-08-30 16:39:31 +0000

    Add mobileBuildUploadIos integration test
    Just adding one more recording and snapshot for `mobile build upload`
    with iOS app file.
    
    Also, moving the skip logic inside `testAutifyCliSnapshot`.

--- integration-test/src/test/helpers/testAutifyCliSnapshot.ts
+++ integration-test/src/test/helpers/testAutifyCliSnapshot.ts
@@ -1,8 +1,26 @@
 /* eslint-disable unicorn/filename-case */
+import { platform } from "node:process";
 import { execAutifyCli } from "../helpers/execAutifyCli";
 
+const isSkip = (command: string) => {
+  // Windows doesn't support autify mobile with iOS app upload so far.
+  if (
+    platform === "win32" &&
+    command.startsWith("mobile") &&
+    command.includes(".app")
+  ) {
+    return true;
+  }
+
+  return false;
+};
+
 export const testAutifyCliSnapshot = (command: string): void => {
-  test(`autify ${command}`, async () => {
-    expect(await execAutifyCli(command)).toMatchSnapshot();
-  }, 300_000);
+  (isSkip(command) ? test.skip : test)(
+    `autify ${command}`,
+    async () => {
+      expect(await execAutifyCli(command)).toMatchSnapshot();
+    },
+    300_000
+  );
 };
