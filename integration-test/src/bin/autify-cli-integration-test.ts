23ec855e20f4bf3237656bc2a65aaae6ca11ed21
Ryosuke Iwanaga <ryosuke.iwanaga@autify.com>
2022-08-30 16:39:31 +0000

    Add mobileBuildUploadIos integration test
    Just adding one more recording and snapshot for `mobile build upload`
    with iOS app file.
    
    Also, moving the skip logic inside `testAutifyCliSnapshot`.

--- integration-test/src/bin/autify-cli-integration-test.ts
+++ integration-test/src/bin/autify-cli-integration-test.ts
@@ -1,5 +1,4 @@
 import { execSync } from "node:child_process";
-import { platform } from "node:os";
 import path from "node:path";
 import { argv } from "node:process";
 
@@ -7,10 +6,7 @@ import { argv } from "node:process";
 const scriptDir = __dirname;
 const rootDir = path.join(scriptDir, "..", "..");
 const isRecord = argv[2] === "--record";
-let command = isRecord ? "test:record" : "test";
-if (platform() === "win32") {
-  command = `${command} -- --testPathIgnorePatterns mobileTestRunIos`;
-}
+const command = isRecord ? "test:record" : "test";
 
 execSync(`npm run ${command}`, {
   stdio: "inherit",
