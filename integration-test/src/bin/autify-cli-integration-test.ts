4978fef7b7e94136e7be8e5fb1378b17c308ce9c
Ryosuke Iwanaga <ryosuke.iwanaga@autify.com>
2022-09-08 08:47:43 +0000

    Allow to run/record subset of integration tests
    Previously, integration test runs/records all test cases.
    For recording especially, we just want to run a few test cases only
    while development.
    
    This commit adds such capability via `autify-cli-integration-test` and
    `npm run test:integration`.
    
    Also, adding convenient commands `npm run test:integration:dev` and
    `npm run test:integration:record:dev`.

--- integration-test/src/bin/autify-cli-integration-test.ts
+++ integration-test/src/bin/autify-cli-integration-test.ts
@@ -1,14 +1,17 @@
 import { execSync } from "node:child_process";
 import path from "node:path";
-import { argv } from "node:process";
+import { argv, env } from "node:process";
 
 // eslint-disable-next-line unicorn/prefer-module
 const scriptDir = __dirname;
 const rootDir = path.join(scriptDir, "..", "..");
-const isRecord = argv[2] === "--record";
+const isRecord = Boolean(env.AUTIFY_CLI_INTEGRATION_TEST_RECORD);
 const command = isRecord ? "test:record" : "test";
+const args = argv.slice(2);
+const testPathPattern =
+  args.length === 0 ? "" : `--testPathPattern '(${args.join("|")})'`;
 
-execSync(`npm run ${command}`, {
+execSync(`npm run ${command} -- ${testPathPattern}`, {
   stdio: "inherit",
   cwd: rootDir,
 });
