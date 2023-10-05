c801be5baa4ee3a6ed64c2ca78788c9ff64ec3d4
Kensei Yarimizu <yarimizu.kensei14@gmail.com>
2023-10-05 15:33:02 +0900

    chore: validate wait interval second

--- src/autify/getWaitIntervalSecond.ts
+++ src/autify/getWaitIntervalSecond.ts
@@ -1,5 +1,6 @@
 /* eslint-disable unicorn/filename-case */
 import { get } from "../config";
+import { CLIError } from "@oclif/errors";
 
 const DEFAULT_INTERVAL_SECOND = 1;
 
@@ -8,6 +9,8 @@ export const getWaitIntervalSecond = (configDir: string): number => {
     get(configDir, "AUTIFY_TEST_WAIT_INTERVAL_SECOND") ??
     String(DEFAULT_INTERVAL_SECOND);
   const waitIntervalSecond = Number.parseFloat(waitIntervalSecondString);
+  if (waitIntervalSecond < 1)
+    throw new CLIError("wait interval second should be greater than 1");
   if (Number.isNaN(waitIntervalSecond)) return DEFAULT_INTERVAL_SECOND;
   return waitIntervalSecond;
 };
