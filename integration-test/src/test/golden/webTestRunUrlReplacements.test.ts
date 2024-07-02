cc0c1c87024236944ecbe9e8bb697c0a037a7c4d
Kensei Yarimizu <yarimizu.kensei14@gmail.com>
2024-07-02 18:00:57 +0900

    fix: escape command line

--- integration-test/src/test/golden/webTestRunUrlReplacements.test.ts
+++ integration-test/src/test/golden/webTestRunUrlReplacements.test.ts
@@ -1,6 +1,13 @@
 /* eslint-disable unicorn/filename-case */
 import { testAutifyCliSnapshot } from "../helpers/testAutifyCliSnapshot";
 
+const command =
+  'web test run https://app.autify.com/projects/0000/scenarios/0000 -r "https://example.com https://example.net?foo=bar"';
+
+const commandForWin =
+  'web test run https://app.autify.com/projects/0000/scenarios/0000 -r """"https://example.com https://example.net?foo=bar""';
+
 testAutifyCliSnapshot(
-  'web test run https://app.autify.com/projects/0000/scenarios/0000 -r "https://example.com https://example.net?foo=bar"'
+  process.platform === "win32" ? commandForWin : command,
+  command
 );
