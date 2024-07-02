b14bd9e30b08f4e24ec58b229966706e1c5fffe1
Kensei Yarimizu <yarimizu.kensei14@gmail.com>
2024-07-02 19:07:22 +0900

    fix

--- integration-test/src/test/golden/webTestRunUrlReplacements.test.ts
+++ integration-test/src/test/golden/webTestRunUrlReplacements.test.ts
@@ -4,10 +4,11 @@ import { testAutifyCliSnapshot } from "../helpers/testAutifyCliSnapshot";
 const command =
   'web test run https://app.autify.com/projects/0000/scenarios/0000 -r "https://example.com https://example.net?foo=bar"';
 
-const commandForWin =
-  'web test run https://app.autify.com/projects/0000/scenarios/0000 -r """"https://example.com https://example.net?foo=bar""';
+// const commandForWin =
+//   'web test run https://app.autify.com/projects/0000/scenarios/0000 -r """"https://example.com https://example.net?foo=bar""';
 
 testAutifyCliSnapshot(
-  process.platform === "win32" ? commandForWin : command,
+  // process.platform === "win32" ? commandForWin : command,
+  command,
   command
 );
