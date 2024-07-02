21ace42247ccb78b541b80825e684f293d2c4b0f
Kensei Yarimizu <yarimizu.kensei14@gmail.com>
2024-07-02 19:41:37 +0900

    fix

--- integration-test/src/test/golden/webTestRunUrlReplacements.test.ts
+++ integration-test/src/test/golden/webTestRunUrlReplacements.test.ts
@@ -1,14 +1,12 @@
 /* eslint-disable unicorn/filename-case */
 import { testAutifyCliSnapshot } from "../helpers/testAutifyCliSnapshot";
 
-const command =
-  'web test run https://app.autify.com/projects/0000/scenarios/0000 -r "https://example.com https://example.net?foo=bar"';
+// const command =
+//   'web test run https://app.autify.com/projects/0000/scenarios/0000 -r "https://example.com https://example.net?foo=bar"';
 
 // const commandForWin =
 //   'web test run https://app.autify.com/projects/0000/scenarios/0000 -r """"https://example.com https://example.net?foo=bar""';
 
 testAutifyCliSnapshot(
-  // process.platform === "win32" ? commandForWin : command,
-  command,
-  command
+  'web test run https://app.autify.com/projects/0000/scenarios/0000 -r "https://example.com https://example.net?foo=bar"'
 );
--- integration-test/src/test/helpers/testAutifyCliSnapshot.ts
+++ integration-test/src/test/helpers/testAutifyCliSnapshot.ts
@@ -1,8 +1,8 @@
 /* eslint-disable unicorn/filename-case */
 import { execAutifyCli } from "../helpers/execAutifyCli";
 
-export const testAutifyCliSnapshot = (command: string, name?: string): void => {
-  test(`autify ${name || command}`, async () => {
+export const testAutifyCliSnapshot = (command: string): void => {
+  test(`autify ${command}`, async () => {
     expect(await execAutifyCli(command)).toMatchSnapshot();
   }, 300_000);
 };
