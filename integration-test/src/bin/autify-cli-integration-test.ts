6c50bb1979a9bd6762cf838efb9df7470f29db8c
tai2 <hello@tai2.net>
2025-03-11 09:32:27 +0900

    Add a comment

--- integration-test/src/bin/autify-cli-integration-test.ts
+++ integration-test/src/bin/autify-cli-integration-test.ts
@@ -15,6 +15,9 @@ execSync(`npm run ${command} -- ${testPathPattern}`, {
   stdio: "inherit",
   env: {
     ...process.env,
+    // The global navigator object brings lots of errors since Node.js doesn't support the onLine property yet.
+    // https://github.com/Netflix/pollyjs/blob/9b6bede12b7ee998472b8883c9dd01e2159e00a8/packages/%40pollyjs/adapter/src/index.js#L186
+    // We need to disable the navigator until Node.js supports it.
     NODE_OPTIONS: "--no-deprecation --no-experimental-global-navigator",
   },
   cwd: rootDir,
