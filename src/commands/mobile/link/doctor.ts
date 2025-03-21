c82e9cc1401bac8570623a66cca668f2f7f18420
tai2 <hello@tai2.net>
2025-03-21 13:23:52 +0900

    Add zarnath-protocol

--- src/commands/mobile/link/doctor.ts
+++ src/commands/mobile/link/doctor.ts
@@ -1,4 +1,4 @@
-import { Command } from "@oclif/core";
+import { Command, Flags } from "@oclif/core";
 import { MobileLinkManager } from "../../../autify/mobile/mobilelink/mobile-link-manager/MobileLinkManager";
 
 export default class MobileLinkDoctor extends Command {
@@ -6,8 +6,19 @@ export default class MobileLinkDoctor extends Command {
   static examples = [
     "Check MobileLink configuration:\n<%= config.bin %> <%= command.id %>",
   ];
+  static flags = {
+    "zarnath-protocol": Flags.integer({
+      description: "Specify the version of zarnath protocol",
+      required: true,
+    }),
+  };
+  static strict = false;
 
   public async run(): Promise<void> {
+    console.log("argv before parse", this.argv);
+    const { flags } = await this.parse(MobileLinkDoctor);
+    console.log("argv after parse", this.argv);
+    console.log("flags", flags);
     const { configDir, cacheDir } = this.config;
 
     const mobileLinkManager = new MobileLinkManager({
