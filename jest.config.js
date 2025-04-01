fcf6d9b43fe0df1ba5d447bfac909e18b98593f3
tai2 <hello@tai2.net>
2025-04-01 13:52:40 +0900

    Fix lint errors

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
+/* eslint-disable  unicorn/prefer-module */
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   testEnvironment: "node",
   transform: {
-    "^.+\.tsx?$": ["ts-jest", {}],
+    "^.+\\.tsx?$": ["ts-jest", {}],
   },
 };
