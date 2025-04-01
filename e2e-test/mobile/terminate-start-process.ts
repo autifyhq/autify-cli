f3e51789ed468762d4126572c486b6f9187d36b1
tai2 <hello@tai2.net>
2025-04-01 11:46:21 +0900

    Add a comment

--- e2e-test/mobile/terminate-start-process.ts
+++ e2e-test/mobile/terminate-start-process.ts
@@ -1,5 +1,7 @@
 import { spawn } from "node:child_process";
 
+// The spawned child process is shell process. It's difficult to get the PID of the actuall mobilelink with Node's
+// ChildProcess API. We insteadl use pkill command to kill mobilelink.
 export const terminateStartProcess = () => {
   spawn("pkill", ["-f", "run link start"]);
 };
