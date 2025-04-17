5f8b98d5f76a73360d2e86a52caef399ac1c2b52
tai2 <hello@tai2.net>
2025-04-17 10:55:01 +0900

    Bump mobilelink 0.3.2

--- src/autify/mobile/mobilelink/installBinary.ts
+++ src/autify/mobile/mobilelink/installBinary.ts
@@ -18,8 +18,8 @@ import { execFile } from "node:child_process";
 import * as tar from "tar";
 import { get } from "../../../config";
 
-const MOBILE_LINK_VERSION = "0.3.1";
-const MOBILE_LINK_HASH = "6de575a31";
+const MOBILE_LINK_VERSION = "0.3.2";
+const MOBILE_LINK_HASH = "ec811f971";
 
 const getOs = () => {
   const os = platform;
